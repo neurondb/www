1b2c202e40db39646c4bf1aafcb951112fd3956b
dev-team <support@neurondb.ai>
2025-12-04 23:49:08 +0500

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -148,6 +148,8 @@ export default function RootLayout({
         {/* Resource hints for performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
         <link rel="dns-prefetch" href="https://github.com" />
         <link rel="dns-prefetch" href="https://twitter.com" />
         
